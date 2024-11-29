0571909b89ff9333cfd4c618da4d0a4d8705c2b3
Egor Chernakov <chernakov.eg@gmail.com>
2024-11-29 23:34:08 +0300

    init

--- cmd/main.go
+++ cmd/main.go
@@ -13,19 +13,30 @@ func main() {
 }
 
 func registerMove(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
 	if r.Method == "OPTIONS" {
+		log.Println("OPTIONS request")
+		w.WriteHeader(204)
+	}
+
+	if r.Method == "POST" {
 		body := make([]byte, 1000)
 		r.Body.Read(body)
-		log.Println("OPTIONS:", string(body))
+		log.Println("POST:", string(body))
+		w.WriteHeader(201)
+	}
 
-		w.Header().Add("Access-Control-Allow-Origin:", "*")
-		w.Header().Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
-		w.Header().Add("Access-Control-Allow-Headers", "X-Requested-With,content-type")
-		w.Header().Add("Access-Control-Allow-Credentials", "true")
+	if r.Method == "GET" {
+		body := make([]byte, 1000)
+		r.Body.Read(body)
+		log.Println("GET:", string(body))
 		w.WriteHeader(200)
 	}
-	if r.Method == "POST" {
-		log.Println("POST")
-	}
-	// w.WriteHeader(201)
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Add("Access-Control-Allow-Origin", "*")
+	(*w).Header().Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH")
+	(*w).Header().Add("Access-Control-Allow-Headers", "X-Requested-With,content-type")
+	(*w).Header().Add("Access-Control-Allow-Credentials", "true")
 }
